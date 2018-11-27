f2530061735fbf0e52e82fadba51de00f6a2ecc6
kenashley2002 <43130055+kenashley2002@users.noreply.github.com>
2018-11-27 13:36:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,8 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, jsonify, request
-import requests
-from bs4 import BeautifulSoup
+from api.get_forexrate import get_forexrate
 import logging
 app = Flask(__name__)
-
-# --- Begin crawler ForexRate ----
-def get_forexrate(url):
-    req = requests.get(url)
-    html = req.text
-    soup = BeautifulSoup(html, 'html.parser')
-    item = []
-    for child in soup.exratelist.find_all('exrate'):
-        item += [{child['currencycode']: [
-            child['currencyname'], child['buy'], child['transfer'], child['sell']
-        ]}]
-    return item
-# --- End crawler Forexrate ---
-
 
 @app.route('/', methods=['GET'])
 def verify():
@@ -32,75 +17,7 @@ def verify():
 
 @app.route('/api/forexrate', methods = ['GET'])
 def forexrate():
-    get_result = get_forexrate('https://www.vietcombank.com.vn/ExchangeRates/ExrateXML.aspx')
-    json_result = {
-        'messages': [
-            {
-                'attachment': {
-                    'type': 'template',
-                    'payload': {
-                        'template_type': 'generic',
-                        'image_aspect_ratio': 'square',
-                        'elements': [
-                            {
-                            'title': get_result[0]['AUD'][0],
-                                "image_url": 'http://blog.buyforexonline.com/wp-content/uploads/2016/07/23463208_s-1.jpg',
-                                'subtitle': 'Mua: ' + str(get_result[0]['AUD'][1]) + ' |' + ' Bán: ' + str(get_result[0]['AUD'][3])
-                            },
-                            {
-                                'title': get_result[1]['CAD'][0],
-                                "image_url": 'http://blog.buyforexonline.com/wp-content/uploads/2016/07/23463208_s-1.jpg',
-                                'subtitle': 'Mua: ' + str(get_result[1]['CAD'][1]) + ' |' + ' Bán: ' + str(
-                                    get_result[1]['CAD'][3])
-                            },
-                            {
-                                'title': get_result[2]['CHF'][0],
-                                "image_url": 'http://blog.buyforexonline.com/wp-content/uploads/2016/07/23463208_s-1.jpg',
-                                'subtitle': 'Mua: ' + str(get_result[2]['CHF'][1]) +' |' + ' Bán: ' + str(
-                                    get_result[2]['CHF'][3])
-                            },
-                            {
-                                'title': get_result[4]['EUR'][0],
-                                "image_url": 'http://blog.buyforexonline.com/wp-content/uploads/2016/07/23463208_s-1.jpg',
-                                'subtitle': 'Mua: ' + str(get_result[4]['EUR'][1]) +' |' + ' Bán: ' + str(
-                                    get_result[4]['EUR'][3])
-                            },
-                            {
-                                'title': get_result[5]['GBP'][0],
-                                "image_url": 'http://blog.buyforexonline.com/wp-content/uploads/2016/07/23463208_s-1.jpg',
-                                'subtitle': 'Mua: ' + str(get_result[5]['GBP'][1]) +' |' + ' Bán: ' + str(
-                                    get_result[5]['GBP'][3])
-                            },
-                            {
-                                'title': get_result[6]['HKD'][0],
-                                "image_url": 'http://blog.buyforexonline.com/wp-content/uploads/2016/07/23463208_s-1.jpg',
-                                'subtitle': 'Mua: ' + str(get_result[6]['HKD'][1]) +' |' + ' Bán: ' + str(
-                                    get_result[6]['HKD'][3])
-                            },
-                            {
-                                'title': get_result[16]['SGD'][0],
-                                "image_url": 'http://blog.buyforexonline.com/wp-content/uploads/2016/07/23463208_s-1.jpg',
-                                'subtitle': 'Mua: ' + str(get_result[16]['SGD'][1]) +' |' + ' Bán: ' + str(
-                                    get_result[16]['SGD'][3])
-                            },
-                            {
-                                'title': get_result[17]['THB'][0],
-                                "image_url": 'http://blog.buyforexonline.com/wp-content/uploads/2016/07/23463208_s-1.jpg',
-                                'subtitle': 'Mua: ' + str(get_result[17]['THB'][1]) +' |' + ' Bán: ' + str(
-                                    get_result[17]['THB'][3])
-                            },
-                            {
-                                'title': get_result[18]['USD'][0],
-                                "image_url": 'http://blog.buyforexonline.com/wp-content/uploads/2016/07/23463208_s-1.jpg',
-                                'subtitle': 'Mua: ' + str(get_result[18]['USD'][1]) +' |' + ' Bán: ' + str(
-                                    get_result[18]['USD'][3])
-                            }
-                        ]
-                    }
-                }
-            }
-        ]
-    }
+    json_result = get_forexrate('https://www.vietcombank.com.vn/ExchangeRates/ExrateXML.aspx')
     return jsonify(json_result)
 
 @app.errorhandler(500)

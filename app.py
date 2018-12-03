01641a677983301a5a6616f08b37fd13d465ee83
kenashley2002 <43130055+kenashley2002@users.noreply.github.com>
2018-12-03 19:59:00 +0700

    v22

--- app.py
+++ app.py
@@ -85,6 +85,7 @@ def goldvn():
 @app.route('/webview/show-buttons', methods = ['GET'])
 def show_buttons():
     userId = request.args.get('userId')
+    global userId
     json_result = get_test_add_cart(userId)
     return jsonify(json_result)
 
@@ -98,7 +99,7 @@ def show_webview():
 @app.route('/webview/broadcast-to-chatfuel', methods = ['POST'])
 def broadcast_to_chatfuel():
     botId = chatfuel_bot_id
-    userId = '1940186179401012'
+    global userId
     token = chatfuel_key
     blockId = '5c03d20676ccbc01968cf102'
     fields = [k for k in request.form]
